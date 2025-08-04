c486cb8afcabb3cdd805225f45c67c1b7268f2de
wampadu <50532644+wampadu@users.noreply.github.com>
2025-08-04 17:59:30 -0400

    Update events_emailer.py

--- events_emailer.py
+++ events_emailer.py
@@ -205,7 +205,7 @@ async def scrape_eventbrite(page):
     start_str = dates[0].strftime("%Y-%m-%d")
     end_str = dates[-1].strftime("%Y-%m-%d")
     url = (
-        "http://api.scraperapi.com"
+        "https://api.scraperapi.com"
         f"?api_key=46f16739ebbc381529ebfd21f01061dd"
         f"&url=https://www.eventbrite.ca/d/canada--toronto/events/?start_date={start_str}&end_date={end_str}"
         "&render=true"
@@ -344,6 +344,7 @@ async def aggregate_events():
 
 if __name__ == "__main__":
     asyncio.run(aggregate_events())
+
 
 
 
