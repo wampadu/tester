3d2f3ff428c86d2369077224b18acbbb806858cd
wampadu <50532644+wampadu@users.noreply.github.com>
2025-08-04 17:27:55 -0400

    Update events_emailer.py

--- events_emailer.py
+++ events_emailer.py
@@ -206,6 +206,7 @@ async def scrape_eventbrite(page):
     end_str = dates[-1].strftime("%Y-%m-%d")
     url = f"https://www.eventbrite.ca/d/canada--toronto/events/?start_date={start_str}&end_date={end_str}"
     await page.goto(url)
+    print(page.content)
 
     while True:
         print("🔄 Scrolling to load events on current page...")
@@ -338,3 +339,4 @@ async def aggregate_events():
 
 if __name__ == "__main__":
     asyncio.run(aggregate_events())
+
